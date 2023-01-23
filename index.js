fddcc50c960511cc773edf049958bc08f797bd18
+ <+>
2023-01-23 14:00:16 -0500

    commit tree 5479f6daafe19f134d7b03e4766be57ade6fbc59

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reverse = "";
+  for (let i = word.length - 1; i >= 0; i--) {
+    reverse += word[i];
+  }
+  return word === reverse;
+
+
 }
 
 /* 
